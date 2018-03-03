426d5d3279517d674704f880f9ec0549ae932eb7
Felix Ebert <felix.ebert@if-core.de>
2018-03-03 18:31:00 +0100

    update cityCouncilEvents

--- functions/cityCouncilEventsIntent.js
+++ functions/cityCouncilEventsIntent.js
@@ -25,24 +25,49 @@ const getNextEventForAssembly = (assembly) => {
 };
 
 const getMessage = (assembly) => {
-    if (!assembly) assembly = "Gemeinderat";
+    if (!assembly) assembly = {
+        id: "Gemeinderat",
+        name: "Gemeinderat"
+    };
     const nextEvent = getNextEventForAssembly(assembly);
 
     switch (nextEvent.state) {
         case 'NOT_AVAILABLE':
             return "Diesen Ausschuss gibt es nicht";
         case 'NO_NEXT_EVENT':
-            return "Es wurde noch kein kommender Termin bekanntgegeben";
+            return "Es wurde noch kein kommender Termin für " + assembly.name + " bekanntgegeben";
         case 'FOUND':
-            return "Das nächste Treffen findet am " + nextEvent.formattedDate + ' statt';
+            return "Das nächste " + assembly.name + " Treffen findet am " + nextEvent.formattedDate + ' statt';
     }
 };
+
+function findResolution(resolutions) {
+    const resolution = resolutions.find((element) => {
+        return (element.status.code === "ER_SUCCESS_MATCH");
+    });
+
+    if (resolution) {
+        return resolution.values[0];
+    } else {
+        return null;
+    }
+}
 
 const getNextEvent = (request, res, shouldEndSession) => {
     console.log("Request: ", request);
     console.log("Intent: ", JSON.stringify(request.intent));
 
-    const result = alexaUtils.createResult(getMessage(), shouldEndSession);
+    const slots = request.intent && request.intent.slots ? request.intent.slots : null;
+    let assembly = null;
+    if(slots) {
+        const assemblySlot = slots.assembly;
+        const assemblySlotValue = findResolution(assemblySlot.resolutions.resolutionsPerAuthority);
+        if(assemblySlotValue && assemblySlotValue.value) {
+            assembly = assemblySlotValue.value;
+        }
+    }
+
+    const result = alexaUtils.createResult(getMessage(assembly), shouldEndSession);
     res.send(result);
 };
 
